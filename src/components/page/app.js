386a504d3f22eb54251ff3bdea2cb85aa7beadf5
Carl Eiserman <carleiserman@gmail.com>
2018-03-21 22:52:14 +0200

    chore: initial commit

---
+++ src/components/page/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/js/dist';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
